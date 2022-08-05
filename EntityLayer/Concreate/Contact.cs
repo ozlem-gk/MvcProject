89a55e894634e86eee2f42736b994c60f914bef9
ozlem goksu <ozlemgoksu4@gmail.com>
2022-08-05 16:20:47 +0300

    Add context class and String Length Attributes

--- EntityLayer/Concreate/Contact.cs
+++ EntityLayer/Concreate/Contact.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,9 +9,16 @@ namespace EntityLayer.Concreate
 {
     public class Contact
     {
+        [Key]
         public int ContactID { get; set; }
+
+        [StringLength(500)]
         public string UserName { get; set; }
+
+        [StringLength(50)]
         public string UserMail { get; set; }
+
+        [StringLength(50)]
         public string Subject { get; set; }
         public string Message { get; set; }
     }
--- EntityLayer/Concreate/Heading.cs
+++ EntityLayer/Concreate/Heading.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,9 +9,14 @@ namespace EntityLayer.Concreate
 {
     public class Heading
     {
+        [Key]
         public int HeadingID { get; set; }
+
+        [StringLength(50)]
         public string HeadingName { get; set; }
         public DateTime HeadingDate { get; set; }
+
+
 
         //connection with Category
         public int CategoryID { get; set; }
